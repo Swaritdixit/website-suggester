4333586a1df674daba4d0ccb4dd232b21ed503f9
Swarit Dixit <swaritdixit351@gmail.com>
2026-05-24 02:03:49 +0530

    routes

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,91 +1,17 @@
-const content=[
-    {
-        title:"Glory",
-        genre:["Action", "Adventure"],
-        mood:["Emotional", "Epic"],
-        type:["K-Drama"],
-        rating:8.8,
-        image:"images/glory.png",
-        platforms:["Netflix","Viki"],
-        description:"A gripping tale of revenge and redemption, Glory follows the journey of a young woman seeking justice in a world filled with corruption."
-    },
-    {
-        title:"Death Note",
-        genre:["Mystery", "Supernatural"],
-        mood:["Dark", "Suspenseful"],   
-        type:["Anime"],
-        rating:9.0,
-        image:"images/deathnote.png",
-        platforms:["Netflix","Crunchyroll"],
-        description:"A high school student discovers a mysterious notebook that grants him the power to kill anyone whose name he writes in it."
-    },
-    {
-        title:"dhurandhar",
-        genre:["Action", "Comedy"],
-        mood:["Light-hearted", "Fun"],
-        type:["Movies"],
-        rating:9.4,
-        image:"images/dhurandhar.png",
-        platforms:["Amazon Prime","Netflix"],
-        description:" a two-part Indian spy thriller duology directed by Aditya Dhar and starring Ranveer Singh. The narrative follows an undercover Indian operative on a high-stakes covert mission to infiltrate Karachi's criminal networks and dismantle terrorist organizations targeting India."
-         
-    },
-    {
-        title:"Modern-family",
-        genre:["Comedy"],
-        mood:["Light-hearted", "Heartwarming"],
-        type:["TV Shows"],
-        rating:8.5,
-        image:"images/modernfamily.png",
-        platforms:["Netflix","ABC"],
-        description:"A comedy series following the lives of a diverse family in Los Angeles."
-    }
-];
+let content=[];
+fetch("http://localhost:3000/movies")
+    .then(response => response.json())
+    .then(data => {
+        content = data;
+        console.log(content);
+    });
 const button=document.getElementById("recommendation");
-const search=document.getElementById("searchInput").value.toLowerCase();
 button.addEventListener("click",()=>{
-    const genre=document.getElementById("genre").value;
-    const mood=document.getElementById("mood").value;
-    const type=document.getElementById("type").value;
-   const filtered = content.filter(item =>
+fetch(`http://localhost:3000/content?search=${search}&genre=${genre}&mood=${mood}&type=${type}`)
+.then(response => response.json())
+.then(filtered => {
 
-    (
-        item.title.toLowerCase().includes(search)
-        ||
-
-        item.description.toLowerCase().includes(search)
-    )
-
-    &&
-
-    (
-        genre === "" ||
-
-        item.genre.some(g =>
-            g.toLowerCase().includes(genre.toLowerCase())
-        )
-    )
-
-    &&
-
-    (
-        mood === "" ||
-
-        item.mood.some(m =>
-            m.toLowerCase().includes(mood.toLowerCase())
-        )
-    )
-
-    &&
-
-    (
-        type === "" ||
-
-        item.type.some(t =>
-            t.toLowerCase().includes(type.toLowerCase())
-        )
-    )
-);
+  
    console.log(filtered );
 const result=document.getElementById("result");
 result.innerHTML="";
@@ -111,7 +37,7 @@ if(filtered.length===0){
 </div>`;
 }
 });
-
+});
 document.getElementById("searchInput").addEventListener("keypress",event=>{
     if(event.key==="Enter"){
         document.getElementById("recommendation").click();
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,64 @@ const express=require('express');
 const cors=require('cors');
 const app=express();
 app.use(cors());
+const content=[  {
+        title:"Glory",
+        genre:["Action","Adventure"],
+        mood:["Emotional","Epic"],
+        type:["K-Drama"],
+        rating:8.8,
+        image:"images/glory.png",
+        platforms:["Netflix","Viki"],
+        description:"A gripping revenge drama."
+    },
+
+    {
+        title:"Death Note",
+        genre:["Mystery","Supernatural"],
+        mood:["Dark","Suspenseful"],
+        type:["Anime"],
+        rating:9.0,
+        image:"images/deathnote.png",
+        platforms:["Netflix","Crunchyroll"],
+        description:"A student discovers a deadly notebook."
+    }
+]
 app.get("/",(req,res)=>{
     res.send("Backend Working");
+});
+app.get("/content",(req,res)=>{
+    const genre=req.query.genre || "";
+    const mood=req.query.mood || "";
+    const type=req.query.type || "";
+    const search=req.query.search || "";
+    const filtered = content.filter(item =>
+    (
+        item.title.toLowerCase().includes(search.toLowerCase())
+        ||
+        item.description.toLowerCase().includes(search.toLowerCase())
+    )
+    &&
+    (
+        genre === "" ||
+        item.genre.some(g =>
+            g.toLowerCase().includes(genre.toLowerCase())
+        )
+    )
+    &&
+    (
+        mood === "" ||
+        item.mood.some(m =>
+            m.toLowerCase().includes(mood.toLowerCase())
+        )
+    )
+    &&
+    (
+        type === "" ||
+        item.type.some(t =>
+            t.toLowerCase().includes(type.toLowerCase())
+        ))
+    );
+    res.json(filtered);
 });
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
